098abb3ada758ff5456374b6ab1be18892b02535
GiuliaGlave <giulia.glave@gmail.com>
2023-03-06 11:17:04 +0100

    prova api

--- src/components/CardsList.vue
+++ src/components/CardsList.vue
@@ -4,19 +4,30 @@ import Card from "./Card.vue";
 
 export default {
   data() {
-    return {};
+    return {
+      cards: [],
+    };
   },
   components: {
     Card,
   },
   created() {
-    console.log(axios);
+    axios
+      .get("https://db.ygoprodeck.com/api/v7/cardinfo.php?num=10&offset=0")
+      .then((response) => {
+        console.log(response); //in console guardo il percoso per accedere all'array con i dati che mi servono
+        this.cards = response.data.data; //riscrivo il percorso
+        console.log(this.cards);
+      });
   },
 };
 </script>
 
 <template>
   <Card />
+  <div v-for="card in cards">
+    {{ card.archetype }}
+  </div>
 </template>
 
 <style lang="scss" scoped></style>
