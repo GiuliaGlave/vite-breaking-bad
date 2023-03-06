83942be85e9c77fe47da7af85f052f1671d7abc0
GiuliaGlave <giulia.glave@gmail.com>
2023-03-06 10:41:42 +0100

    first commit

---
+++ src/components/AppMain.vue
@@ -0,0 +1,17 @@
+<script>
+import CardsList from "./CardsList.vue";
+
+export default {
+  components: {
+    CardsList,
+  },
+};
+</script>
+
+<template>
+  <section>
+    <CardsList />
+  </section>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ src/components/CardsList.vue
@@ -0,0 +1,22 @@
+<script>
+import axios from "axios";
+import Card from "./Card.vue";
+
+export default {
+  data() {
+    return {};
+  },
+  components: {
+    Card,
+  },
+  created() {
+    console.log(axios);
+  },
+};
+</script>
+
+<template>
+  <Card />
+</template>
+
+<style lang="scss" scoped></style>
